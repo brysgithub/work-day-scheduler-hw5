2fd09335c7b002707b060902c8d2e7292d08ea03
goblin.brn <bschenck0@gmail.com>
2021-12-14 20:52:33 -0800

    updated colorInput function to get the hours color coded correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,32 @@ $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
 var currentTime = moment().format("LT");
 console.log(currentTime);
-console.log(currentTime[0]);
+var currentTime24Hours = moment().format("HH");
+console.log(currentTime24Hours);
 
 function colorInput() {
-    var textInputEL = document.getElementsByClassName("textInput");
-    var hourEl = document.getElementsByClassName("hour");
-
-    // TODO: figure out the right array to pull "array.length" from
-    for (let i = 0; i < array.length; i++) {
+    
+    for (let i = 9; i < 18; i++) {
+        
+        var textInputEl = $('[data-id="' + i + '"]');  
         // TODO: add if else statements to make the timeblocks the correct color
-        if (hourEl == currentTime[0]) {
-            textInputEL.classList.add("present");
+
+        if (i == currentTime24Hours) {
+            textInputEl.addClass('present');
             console.log("present");
-        } else if (hourEl > currentTime[0]) {
-            textInputEL.classList.add("future");
+        } else if (i > currentTime24Hours) {
+            textInputEl.addClass('future');
             console.log("future");
         } else {
-            textInputEL.classList.add("past");
+            textInputEl.addClass('past');
             console.log("past");
         };
     };
 };
 
+colorInput();
+
 //TODO: save user input to local storage to recall on page reload
 function saveInput () {
-
+    //save things heererereerer
 };
